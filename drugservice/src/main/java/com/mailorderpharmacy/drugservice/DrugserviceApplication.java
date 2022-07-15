d9ae064a16dad5caf7e044540e5a8a8b238e69d6
nitinagrawaloff <nitinagrawaloff@gmail.com>
2022-07-15 17:58:55 +0530

    Initial Commit

---
+++ drugservice/src/main/java/com/mailorderpharmacy/drugservice/DrugserviceApplication.java
@@ -0,0 +1,18 @@
+package com.mailorderpharmacy.drugservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableSwagger2
+public class DrugserviceApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(DrugserviceApplication.class, args);
+	}
+
+}
---
+++ drugservice/src/test/java/com/mailorderpharmacy/drugservice/DrugserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailorderpharmacy.drugservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
